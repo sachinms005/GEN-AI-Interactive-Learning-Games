b5c7d61968946db7420743200f64cb857201ea67
SACHIN M S <sachinmurugesh20@gmail.com>
2025-04-04 21:23:54 +0530

    Delete backendroutesstoryRoutes.js

--- backendroutesstoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/generate", async (req, res) => {
-  const story = "Once upon a time in the AI learning world...";
-  res.json({ story });
-});
-
-module.exports = router;
